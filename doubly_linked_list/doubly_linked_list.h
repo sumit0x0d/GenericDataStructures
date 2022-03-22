53056f6103e42ae53ce0c78aa636dc25142dd0ae
Sumit Singh <sumit@determinantstudios.com>
2022-03-22 15:58:21 +0530

    test

--- doubly_linked_list/doubly_linked_list.h
+++ doubly_linked_list/doubly_linked_list.h
@@ -21,12 +21,9 @@ typedef struct DoublyLinkedList {
 DoublyLinkedList doubly_linked_list_construct();
 void doubly_linked_list_destruct(DoublyLinkedList *DLL);
 
-    Q.front = 0;
-    Q.back = 0;DoublyLinkedListNode *doubly_linked_list_front(DoublyLinkedList *DLL);
-    Q.front = 0;
-    Q.back = 0;DoublyLinkedListNode *doubly_linked_list_back(DoublyLinkedList *DLL);
-    Q.front = 0;
-    Q.back = 0;size_t doubly_linked_list_size(DoublyLinkedList *DLL);
+inline DoublyLinkedListNode *doubly_linked_list_front(DoublyLinkedList *DLL);
+inline DoublyLinkedListNode *doubly_linked_list_back(DoublyLinkedList *DLL);
+inline size_t doubly_linked_list_size(DoublyLinkedList *DLL);
 
 DoublyLinkedListNode *doubly_linked_list_search(DoublyLinkedList *DLL, void *data, size_t data_type_size);
 bool doubly_linked_list_push_front(DoublyLinkedList *DLL, void *data, size_t data_type_size);
