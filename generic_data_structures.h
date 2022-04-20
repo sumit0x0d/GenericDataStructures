6ea05f27bc64b4d4b2a054f2c1d137d8f15f084d
Sumit Singh <sumit@determinantstudios.com>
2022-04-21 01:55:20 +0530

    test

--- generic_data_structures.h
+++ generic_data_structures.h
@@ -1,27 +1,20 @@
 #ifndef GENERIC_DATA_STRUCTURES_H
 #define GENERIC_DATA_STRUCTURES_H
 
-#include "dynamic_array/dynamic_array.h"
-
-#include "deque-array/deque-array.h"
-#include "deque-linked_list/deque-linked_list.h"
-
-#include "queue-array/queue-array.h"
-#include "queue-linked_list/queue-linked_list.h"
-
-#include "doubly_linked_list/doubly_linked_list.h"
-#include "singly_linked_list/singly_linked_list.h"
-
-#include "stack-array/stack-array.h"
-#include "stack-linked_list/stack-linked_list.h"
-
 #include "avl_tree/avl_tree.h"
 #include "binary_search_tree/binary_search_tree.h"
-#include "red-black_tree/red-black_tree.h"
-
+#include "deque-array/deque-array.h"
+#include "deque-linked_list/deque-linked_list.h"
+#include "doubly_linked_list/doubly_linked_list.h"
+#include "dynamic_array/dynamic_array.h"
 #include "graph-adjacency_list/graph-adjacency_list.h"
 #include "graph-adjacency_matrix/graph-adjacency_matrix.h"
-
 #include "hash_table-separate_chaining/hash_table-separate_chaining.h"
+#include "queue-array/queue-array.h"
+#include "queue-linked_list/queue-linked_list.h"
+#include "red-black_tree/red-black_tree.h"
+#include "singly_linked_list/singly_linked_list.h"
+#include "stack-array/stack-array.h"
+#include "stack-linked_list/stack-linked_list.h"
 
 #endif
